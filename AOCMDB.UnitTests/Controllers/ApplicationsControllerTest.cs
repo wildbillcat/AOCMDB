aecb3ba32bfcc09133a3a2f9015b0f2bbf05230b
Patrick McMorran <kingrolloinc@sbcglobal.net>
2015-04-15 22:11:04 -0400

    Started implementing MSTest for the Controllers.

---
+++ AOCMDB.UnitTests/Controllers/ApplicationsControllerTest.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Web.Mvc;
+using AOCMDB.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AOCMDB.UnitTests
+{
+    [TestClass]
+    public class ApplicationsControllerTest
+    {
+        [TestMethod]
+        public void IndexActionReturnsIndexView()
+        {
+            string expected = "Index";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void DetailsActionReturnsDetailsViewValidIDAndVersion()
+        {
+            throw new NotImplementedException();
+            string expected = "Details";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void DetailsActionReturnsDetailsViewInValidIDOrVersion()
+        {
+            throw new NotImplementedException();
+            string expected = "Details";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void DetailsActionReturnsDetailsViewInValidIDOrVersion()
+        {
+            throw new NotImplementedException();
+            string expected = "Details";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void CreateActionReturnsCreateViewGET()
+        {
+            throw new NotImplementedException();
+            string expected = "Create";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void CreateActionReturnsCreateViewPOST()
+        {
+            throw new NotImplementedException();
+            string expected = "Create";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void CreateActionReturnsCreateViewPOST()
+        {
+            throw new NotImplementedException();
+            string expected = "Create";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void ContactActionReturnsContactView()
+        {
+            throw new NotImplementedException();
+            string expected = "Details";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Details() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void ContactActionReturnsHttpNotFoundView()
+        {
+            throw new NotImplementedException();
+            string expected = "Details";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Details() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void ContactActionReturnsContactView()
+        {
+            throw new NotImplementedException();
+            string expected = "Edit";
+            ApplicationsController controller = new ApplicationsController();
+
+            var result = controller.Edit() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+    }
+}
---
+++ AOCMDB.UnitTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using AOCMDB.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AOCMDB.UnitTests
+{
+    [TestClass]
+    public class HomeControllerControllerTest
+    {
+        [TestMethod]
+        public void IndexActionReturnsIndexView()
+        {
+            string expected = "Index";
+            HomeController controller = new HomeController();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void AboutActionReturnsAboutView()
+        {
+            string expected = "About";
+            HomeController controller = new HomeController();
+
+            var result = controller.About() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+
+        [TestMethod]
+        public void ContactActionReturnsContactView()
+        {
+            string expected = "Contact";
+            HomeController controller = new HomeController();
+
+            var result = controller.Contact() as ViewResult;
+
+            Assert.AreEqual(expected, result.ViewName);
+        }
+    }
+}
