2e7dd51b33223e39aaee9460dd3632356ee5c116
Patrick McMorran <kingrolloinc@sbcglobal.net>
2015-07-06 22:41:30 -0400

    started scaffolding version 1 api interfaces. Need to complete paths and scaffold additonal model interfaces

--- AOCMDB/App_Start/WebApiConfig.cs
+++ AOCMDB/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace AOCMDB
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/v{APIVersion}/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ AOCMDB/Controllers/api/Data/IDependenciesControllerAPIV1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOCMDB.Controllers.api.Data
+{
+    /// <summary>
+    /// This will note the API methods required of any controller that exposes the Version 1
+    /// API of Dependencies.
+    /// </summary>
+    interface IDependenciesControllerAPIV1<T>
+    {
+        ICollection<T> Get(string APIVersion);
+
+        T Get(string APIVersion, long Id);
+
+        string GetName(string APIVersion, long Id);
+
+        string GetFriendlyName(string APIVersion, long Id);
+
+        string GetDetails(string APIVersion, long Id);
+
+        string SetName(string APIVersion, long Id, string Value);
+
+        string SetFriendlyName(string APIVersion, long Id, string Value);
+
+        string SetDetails(string APIVersion, long Id, string Value);
+
+        ICollection<T> GetDownstreamDependencies(string APIVersion);
+
+        T GetDownstreamDependencies(string APIVersion, long Id);
+
+        void AddDownStreamDependency(string APIVersion, long Id, long DownStreamId);
+
+    }
+}
